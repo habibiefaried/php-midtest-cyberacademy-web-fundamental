1146b4caf6c7e8bc6496e6f3d4a98a08f11fd50d
Habibie Faried <habibiefaried@gmail.com>
2021-10-05 08:32:54 +0400

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ $input = str_replace('`', '', $input);
 $input = str_replace('$', '', $input);
 $input = str_replace('cp', '', $input);
 $input = str_replace('mv', '', $input);
+$input = str_replace('sh', '', $input);
 
 system("ls \"$(echo \"".$input."\")\" ");
