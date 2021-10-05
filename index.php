f232e9030733dc1cf2ea2bd622983f46bcdfcb01
Habibie Faried <habibiefaried@gmail.com>
2021-10-05 08:30:28 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ $input = $_GET['dir'];
 // Sanitasi
 $input = str_replace('`', '', $input);
 $input = str_replace('$', '', $input);
+$input = str_replace('cp', '', $input);
+$input = str_replace('mv', '', $input);
 
 system("ls \"$(echo \"".$input."\")\" ");
